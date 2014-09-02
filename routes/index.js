260975cb286bd312ab5285c04b4aaacc8335cdf8
Edudjr <eduardo.djr@hotmail.com>
2014-09-02 05:33:03 -0400

    updated live page. reloads page every 10 seconds

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var feedr = require('./superfeedr');
 var router = express.Router();
+var last_item;
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -22,7 +23,27 @@ router.post('/live',function(req, res){
 	};
 	
 	feedr.retrieve(send, function(data){
+		last_item = data.items[0].title;
+		console.log("last item: ");
+		console.log(last_item);
 		res.send(data);
+	});
+});
+
+router.post('/load',function(req, res){
+	var send = {
+		url:"http://www.theverge.com/rss/index.xml",
+		count:1
+	};
+	
+	feedr.retrieve(send, function(data){
+		console.log(data.items[0].title);
+		console.log(last_item);
+		console.log(data.items[0].title===last_item)
+		if(data.items[0].title===last_item)
+			res.send("err");
+		else
+			data.items[0].title;
 	});
 });
 
